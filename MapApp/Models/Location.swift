97fca0a87ad0dd8b09493bd462c13961031b76bb
Manuel <manuel@manuelparcon.com>
2024-10-05 17:25:11 +0800

    [Feature] Added Custom Data Model and Data

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by Manuel Parcon on 10/5/24.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
